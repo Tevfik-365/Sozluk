5fd518d4399e735bfe02ed39e8c8e57fe2c3e7f8
Tevfik <tevfiktzn@gmail.com>
2024-10-28 12:54:44 +0300

    Update and rename sozluk.py to dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+meme_dict = {
+"CRINGE": "Something strange or embarrassing",
+"LOL": "A response to something funny",
+"MINECRFT" : "The craft is mine",
+'ROLEPLAY' : 'Role play',
+'EZ' : 'To humiliate an opponent',
+'AFK' : 'Taking a break from the game when the game starts'
+}
+word = input("enter words")
+if word in meme_dict.keys():
+print(meme_dict[kelime])
+else:
+print('word not in list')
